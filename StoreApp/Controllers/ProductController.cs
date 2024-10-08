4a9f0d2cf11ed9a238c48d5b0ed82ea9270a75df
Isa Albayrak <isaalbayrak66@icloud.com>
2024-10-08 18:15:13 +0300

    Add project files.

---
+++ StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IEnumerable<Product> Index()
+        {
+            return new List<Product>()
+            {
+                new Product() { ProductId = 1, ProductName = "Computer", Price = 5 }
+            };
+        }
+    }
+}
---
+++ StoreApp/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
