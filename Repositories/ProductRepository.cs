60310c03c2bc851c181d883a51357bc645542d7e
Isa Albayrak <isaalbayrak66@icloud.com>
2024-10-13 16:19:48 +0300

    Repository ve category islemleri

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Repositories.Contracts;
+
+namespace Repositories
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(RepositoryContext context) : base(context)
+        {
+        }
+        public IQueryable<Product> GetAllProducts(bool trackChanges)=> FindAll(trackChanges);
+
+        //Interface 
+
+        public Product? GetOneProduct(int id,bool trackChanges)
+        {
+            return FindByCondition(p => p.ProductId.Equals(id), trackChanges);
+        }
+    }
+        
+}
