70b9e158acac000d2e6b902acf0af6564fd8ce3c
Isa Albayrak <isaalbayrak66@icloud.com>
2024-10-30 18:17:46 +0300

    _Page Model ,  _Cart Tanımı yapildi ve sepet islemleri tamamlandi.

---
+++ Entities/Models/Cart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; }
+        
+        public Cart()
+        {
+            Lines = new List<CartLine>();
+        }     
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine? line = Lines.Where(line=> line.Product.ProductId.Equals(product.ProductId))
+                       .FirstOrDefault();
+            if (line == null)
+            {
+                Lines.Add(new CartLine()
+                {
+                    Product = product,
+                    Quantity = quantity
+                }); 
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product) =>
+        Lines.RemoveAll(line => line.Product.ProductId.Equals(product.ProductId));
+
+        public decimal ComputeTotalValue()=> 
+            Lines.Sum(e=>e.Product.Price*e.Quantity);
+        public void Clear()=>Lines.Clear();
+
+    }
+}
